28d4bf8c156350739fc0a61996e7960eecb328a4
Jvictor903 <zepvip.sousa@gmail.com>
2024-12-02 10:05:09 -0300

    alteracao

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,17 +1,35 @@
+<<<<<<< HEAD
 document.getElementById("feedbackForm").addEventListener("submit", function(event) {
   event.preventDefault();
+=======
+let btnEnviar = document.getElementById("validar")
+function validarEnvio (){
+>>>>>>> 5e2b9da ( alteracao)
 
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const message = document.getElementById("message").value.trim();
 
   if (!name || !email || !message) {
+<<<<<<< HEAD
       alert("Por favor, preencha todos os campos.");
       return;
+=======
+    alert("Por favor, preencha todos os campos.");
+    return;
+>>>>>>> 5e2b9da ( alteracao)
   }
   const feedbackMessage = document.getElementById("feedbackMessage");
   feedbackMessage.innerText = "Obrigado pelo feedback!";
   feedbackMessage.style.color = "green";
+<<<<<<< HEAD
 
   document.getElementById("feedbackForm").reset();
 });
+=======
+  
+  document.getElementById("feedbackForm").reset();
+
+}
+btnEnviar.addEventListener ('click', validarEnvio)
+>>>>>>> 5e2b9da ( alteracao)
